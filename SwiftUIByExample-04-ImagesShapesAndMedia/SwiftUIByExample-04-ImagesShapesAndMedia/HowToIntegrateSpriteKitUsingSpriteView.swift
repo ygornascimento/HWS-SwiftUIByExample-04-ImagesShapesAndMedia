1ae82e83c5588be045ccefe7cc17a2b05a22d6e8
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-11-26 16:33:26 -0300

    Done - How to integrate SpriteKit using SpriteView

---
+++ SwiftUIByExample-04-ImagesShapesAndMedia/SwiftUIByExample-04-ImagesShapesAndMedia/HowToIntegrateSpriteKitUsingSpriteView.swift
@@ -0,0 +1,44 @@
+//
+//  HowToIntegrateSpriteKitUsingSpriteView.swift
+//  SwiftUIByExample-04-ImagesShapesAndMedia
+//
+//  Created by Ygor Nascimento on 26/11/23.
+//
+
+import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+
+struct HowToIntegrateSpriteKitUsingSpriteView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    var body: some View {
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    HowToIntegrateSpriteKitUsingSpriteView()
+}
